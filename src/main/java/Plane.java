a33c908e1fca1cf93892ae9adb1b013aa22ed050
peaky76 <robertjamespeacock@gmail.com>
2020-08-28 11:53:23 +0100

    Plane: contructor with planeType set up, tested

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,21 @@
 public class Plane {
+
+    private PlaneType planeType;
+
+    public Plane(PlaneType planeType) {
+        this.planeType = planeType;
+    }
+
+    public PlaneType getPlaneType() {
+        return planeType;
+    }
+
+    public int capacity() {
+        return planeType.getCapacity();
+    }
+
+    public int maxWeight() {
+        return planeType.getMaxWeight();
+    }
+
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING777);
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(400, plane.capacity());
+    }
+
+    @Test
+    public void hasMaxWeight() {
+        assertEquals(20000, plane.maxWeight());
+    }
+
 }
