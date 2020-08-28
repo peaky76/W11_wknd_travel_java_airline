c66bd4921f6d42a4d7a5d68d33a39ddbd82c39d6
peaky76 <robertjamespeacock@gmail.com>
2020-08-28 11:06:18 +0100

    Passenger: constructor with name, bagCount set up and tested

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,18 @@
 public class Passenger {
+
+    private String name;
+    private int bagCount;
+
+    public Passenger(String name, int bagCount) {
+        this.name = name;
+        this.bagCount = bagCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBagCount() {
+        return bagCount;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Fred Bloggs", 2);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fred Bloggs", passenger.getName());
+    }
+
+    @Test
+    public void hasBagCount() {
+        assertEquals(2, passenger.getBagCount());
+    }
 }
